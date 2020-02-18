e6673e833c6d68f1fd53ccd602dfa193582be446
Piotr Drożdż <piotr.drozdz0@op.pl>
2020-02-18 20:12:57 +0100

    feat: add oldcamp map

--- src/app/worlds/consts/map-images.ts
+++ src/app/worlds/consts/map-images.ts
@@ -6,7 +6,12 @@ export const mapImages: MapImages = {
   blank: {
     imageUrl: `${directory}BLANK.PNG`,
     name: 'BLANK',
-    bounds: [[-325, -475], [325, 475]],
+    bounds: [[-48750, -71250], [48750, 71250]],
+  },
+  world: {
+    imageUrl: `${directory}MAP_WORLD.PNG`,
+    name: 'MAP_WORLD',
+    bounds: [[-330, -480], [320, 470]],
   },
   worldHD: {
     imageUrl: `${directory}MAP_WORLD_HD.PNG`,
@@ -17,5 +22,10 @@ export const mapImages: MapImages = {
     imageUrl: `${directory}MAP_TEMPLE.PNG`,
     name: 'MAP_TEMPLE',
     bounds: [[-87, -115], [208, 285]],
+  },
+  oldcamp: {
+    imageUrl: `${directory}MAP_OLDCAMP.PNG`,
+    name: 'MAP_OLDCAMP',
+    bounds: [[-55, -57], [40, 53]],
   },
 };
